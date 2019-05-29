3e70530c4a80d8d4077c3dc1f6763fe05206d811
Jeff Gonzalez <jeff@Hypertheory>
2019-05-29 15:09:47 -0400

    done with tip calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'few200';
+  title = 'Front-End Web 200 With Angular!!';
+
 }
