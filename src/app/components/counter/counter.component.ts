46b77f49d7a2821783c1ac2196561ea2dd8869b9
Jeff Gonzalez <jeff@Hypertheory>
2019-05-31 11:21:12 -0400

    before lunch

--- src/app/components/counter/counter.component.ts
+++ src/app/components/counter/counter.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { State, selectCurrentCount } from '../../reducers';
+import { Store, Action } from '@ngrx/store';
+import { State, selectCurrentCount, selectAtTheStart, selectCountingBy, selectCantDecrement } from '../../reducers';
 import { Observable } from 'rxjs';
-
+import * as counterActions from '../../actions/counter.actions';
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
@@ -11,22 +11,32 @@ import { Observable } from 'rxjs';
 export class CounterComponent implements OnInit {
 
   count$: Observable<number>;
+  cantDecrement$: Observable<boolean>;
+  by$: Observable<number>;
+  atTheStart$: Observable<boolean>;
   constructor(private store: Store<State>) { }
 
   ngOnInit() {
     this.count$ = this.store.select(selectCurrentCount);
+    this.by$ = this.store.select(selectCountingBy);
+    this.cantDecrement$ = this.store.select(selectCantDecrement);
+    this.atTheStart$ = this.store.select(selectAtTheStart);
   }
 
   increment() {
+
     // this.count = this.count + 1;
-    this.store.dispatch({ type: 'increment' });
+    this.store.dispatch(new counterActions.CountIncremented());
   }
 
   decrement() {
-    this.store.dispatch({ type: 'decrement' });
+    this.store.dispatch(new counterActions.CountDecremented());
   }
   reset() {
-    this.store.dispatch({ type: 'reset' });
+    this.store.dispatch(new counterActions.CountReset());
   }
 
+  setCountBy(what: number) {
+    this.store.dispatch(new counterActions.CountBySet(what));;
+  }
 }
